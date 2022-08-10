70671d681754c91384ad1e5f555ed0ae46ae8229
voideus <tbikash227@gmail.com>
2022-08-10 19:52:38 +0545

    Sentry integration:

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.18
 
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/getsentry/sentry-go v0.13.0
 	github.com/gin-gonic/gin v1.8.1
 	github.com/joho/godotenv v1.4.0
 	golang.org/x/crypto v0.0.0-20220722155217-630584e8d5aa
--- route/api.go
+++ route/api.go
@@ -1,9 +1,15 @@
 package route
 
 import (
+	"log"
 	"net/http"
+	"os"
+	"time"
 
+	"github.com/getsentry/sentry-go"
+	sentrygin "github.com/getsentry/sentry-go/gin"
 	"github.com/gin-gonic/gin"
+
 	"github.com/voideus/mini-ecommerce/handler"
 	"github.com/voideus/mini-ecommerce/middleware"
 )
@@ -11,13 +17,47 @@ import (
 func RunAPI(address string) error {
 	userHandler := handler.NewUserHandler()
 
-	r := gin.Default()
+	err := sentry.Init(sentry.ClientOptions{
+		Dsn: os.Getenv("SENTRY_DSN"),
+	})
+	if err != nil {
+		log.Fatalf("sentry.Init: %s", err)
+	}
 
-	r.GET("/", func(ctx *gin.Context) {
+	app := gin.Default()
+
+	app.Use(sentrygin.New(sentrygin.Options{
+		Repanic:         true,
+		WaitForDelivery: false,
+		Timeout:         time.Minute,
+	}))
+
+	app.Use(func(ctx *gin.Context) {
+		if hub := sentrygin.GetHubFromContext(ctx); hub != nil {
+			hub.Scope().SetTag("someRandomTag", "maybeWeNeedIt")
+		}
+		ctx.Next()
+	})
+
+	app.GET("/", func(ctx *gin.Context) {
+		if hub := sentrygin.GetHubFromContext(ctx); hub != nil {
+			hub.WithScope(func(scope *sentry.Scope) {
+				scope.SetExtra("unwantedQuery", "someQueryDataMaybe")
+				hub.CaptureMessage("User provided unwanted query string, but we recovered just fine")
+			})
+		}
+
 		ctx.String(http.StatusOK, "Welcome to mini ecommerce")
 	})
 
-	apiRoutes := r.Group("/api")
+	apiRoutes := app.Group("/api")
+
+	app.GET("/foo", func(ctx *gin.Context) {
+		// sentrygin handler will catch it just fine. Also, because we attached "someRandomTag"
+		// in the middleware before, it will be sent through as well
+		panic("y tho again")
+	})
+
 	userRoutes := apiRoutes.Group("/user")
 	{
 		userRoutes.POST("/register", userHandler.AddUser)
@@ -31,5 +71,5 @@ func RunAPI(address string) error {
 		userProtectedRoutes.GET("/:id/products", userHandler.GetProductOrdered)
 	}
 
-	return r.Run(address)
+	return app.Run(address)
 }
